d862b52a8cd1de48655f4397a969d750efa67e7e
AKBARSHOKH09 <muhammadjonovakbar073@gmail.com>
2024-11-25 16:54:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let array=[2,4,9,10,45,67,8,90] 
+
+for( i=0; i<array.length; i++) {
+    if(array[i]%2==0){
+        console.log(array[i]);
+        
+    }
+}
+
+
+// for(let i=0; i<25; i++) {
+//     if (i%2==0) {
+//         console.log(i);
+        
+//     }
+// }
